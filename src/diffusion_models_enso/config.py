88ede380e4dfb6cb49a753d4dd1e45c2b1a97be3
Niccolo Zagli <niccolo.zagli@su.se>
2025-05-13 16:44:28 +0200

    Fixed the environment

--- src/diffusion_models_enso/config.py
+++ src/diffusion_models_enso/config.py
@@ -1,7 +1,7 @@
-# from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings
 
 
-class DataRetrievinSettings:  # (BaseSettings):
+class DataRetrievinSettings(BaseSettings):
     ENSO_lat_min: float = -5
     ENSO_lat_max: float = +5
     ENSO_long_min: float = 190
